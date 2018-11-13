1e1aabf10c7ba4c381e8e5ebde707fdb31e6a6a3
Justin <justin.akkerman@gmail.com>
2018-11-13 16:03:34 +1100

    Initial Commit

---
+++ coreABM.R
@@ -0,0 +1,30 @@
+#This is the model of the processes that are in Books 1-3 of the 
+#Traveller game that came out in the late seventies. This will
+#use agent based modelling to explore the survival of captains 
+#and their ships from bankruptcy. Can free traders survive? If
+#not, what constraints need to be relaxed for them to do so.
+#this project is to learn how to design and implement an ABM 
+#and to explore the solution space for free trader survival
+
+## Function Set Up
+destList <- function(portLocation, jumpRange) {
+  #match planet and return the list of destinations within 
+  #jump range
+} return (destinationsList)
+
+cargoList <- function (cargoSource, jumpRange) {
+  #Determine the ports of Call
+  portsOfCall <- destList(cargoSource, jumpRange)
+  
+  
+  # lookup the UPPs for the two points and generate the 
+  # lots and tonnage going between the two ports
+  
+} return (pointToPointCargos)
+
+## Initialise
+
+## Random Activiation
+
+## Post turn Admin
+
