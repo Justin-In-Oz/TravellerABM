fb04817806ceb111b5b8e89a169d96300b347c89
Justin <justin.akkerman@gmail.com>
2019-01-04 14:37:41 +1100

    completed the resale DMs and begun checking

--- specTrade.R
+++ specTrade.R
@@ -68,6 +68,8 @@ poorWorldResaleDM <- vector(mode = "numeric", length = 36)
 qtyDice <- vector(mode = "numeric", length = 36)
 qtyMult <- vector(mode = "numeric", length = 36)
 
+# Purchase DMs 
+{
 # set the agrWorldPurch Mods
 agrWorldPurchDM[1]  <- -7 # 11 Textiles
 agrWorldPurchDM[3]  <- -4 # 13 Liquor
@@ -79,8 +81,8 @@ agrWorldPurchDM[17] <- -2 # 35 Fruit
 
 # set the Non Agricultural world Purch mods
 nonAgrWorldPurchDM[1]  <- -5 # 11 Textiles
-nonAgrWorldPurchDM[5]  <- -5 # 15 Crystals
-nonAgrWorldPurchDM[13] <- -5 # 31 Petrochemicals
+nonAgrWorldPurchDM[5]  <- -3 # 15 Crystals
+nonAgrWorldPurchDM[13] <- -4 # 31 Petrochemicals
 nonAgrWorldPurchDM[14] <-  1 # 32 Grain
 nonAgrWorldPurchDM[15] <-  2 # 33 Meat
 nonAgrWorldPurchDM[16] <-  3 # 34 Spices
@@ -157,7 +159,7 @@ richWorldPurchDM[34] <- -3 # 64 Computer Parts
 richWorldPurchDM[35] <- -4 # 65 Machine Tools
 richWorldPurchDM[36] <- -1 # 66 Vacc Suits
 
-# set the Rich world Purch mods
+# set the Poor world Purch mods
 poorWorldPurchDM[2]  <-  2 # 12 Polymers
 poorWorldPurchDM[7]  <-  1 # 21 Steel
 poorWorldPurchDM[8]  <-  1 # 22 Copper
@@ -173,6 +175,10 @@ poorWorldPurchDM[23] <-  3 # 45 Tools
 poorWorldPurchDM[24] <-  3 # 46 Body Armour
 poorWorldPurchDM[29] <-  4 # 55 AFV
 
+} # End of Purchase DMs
+
+# Resale DMs
+{
 # set the Agricultural world Resale mods
 agrWorldResaleDM[1]  <- -6 # 11 Textiles
 agrWorldResaleDM[3]  <- -3 # 13 Liquor
@@ -195,4 +201,97 @@ nonAgrWorldResaleDM[34] <-  2 # 64 Computer Parts
 nonAgrWorldResaleDM[35] <-  2 # 65 Machine Tools
 
 # set the Industrial world Resale mods
-indWorldResaleDM[2]  <- -2 # Polymers
+indWorldResaleDM[2]  <- -2 # 12 Polymers
+indWorldResaleDM[3]  <-  1 # 13 Liquor
+indWorldResaleDM[4]  <-  1 # 14 Wood
+indWorldResaleDM[5]  <-  3 # 15 Crystals
+indWorldResaleDM[6]  <-  6 # 16 Radioactives
+indWorldResaleDM[7]  <- -2 # 21 Steel
+indWorldResaleDM[8]  <- -3 # 22 Copper
+indWorldResaleDM[9]  <- -3 # 23 Aluminium
+indWorldResaleDM[10] <- -3 # 24 Tin
+indWorldResaleDM[11] <-  5 # 25 Silver
+indWorldResaleDM[12] <- -3 # 26 Special Alloys
+indWorldResaleDM[13] <-  3 # 31 Petrochemicals
+indWorldResaleDM[15] <-  2 # 33 Meat
+indWorldResaleDM[17] <-  3 # 35 Fruit
+indWorldResaleDM[18] <-  5 # 36 Pharmaceuticals
+indWorldResaleDM[19] <-  4 # 41 Gems
+indWorldResaleDM[20] <- -2 # 42 Firearms
+indWorldResaleDM[21] <- -2 # 43 Ammunition
+indWorldResaleDM[22] <- -2 # 44 Blades
+indWorldResaleDM[23] <- -2 # 45 Tools
+indWorldResaleDM[24] <- -2 # 46 Body Armour
+
+# set the Non-Industrial world Resale mods
+nonIndWorldResaleDM[6]  <- -3 # 16 Radioactives
+nonIndWorldResaleDM[9]  <-  4 # 23 Aluminium
+nonIndWorldResaleDM[12] <-  4 # 26 Special Alloys
+nonIndWorldResaleDM[13] <- -5 # 31 Petrochemicals
+nonIndWorldResaleDM[19] <- -2 # 41 Gems
+nonIndWorldResaleDM[25] <-  2 # 51 Aircraft
+nonIndWorldResaleDM[26] <-  2 # 52 Air/Raft
+nonIndWorldResaleDM[27] <-  2 # 53 Computers
+nonIndWorldResaleDM[28] <-  2 # 54 ATV
+nonIndWorldResaleDM[31] <-  2 # 61 Electronics Parts
+nonIndWorldResaleDM[32] <-  3 # 62 Mechanical Parts
+nonIndWorldResaleDM[33] <-  4 # 63 Cybernetic Parts
+nonIndWorldResaleDM[34] <-  3 # 64 Computer Parts
+nonIndWorldResaleDM[35] <-  3 # 65 Machine Tools
+nonIndWorldResaleDM[36] <-  2 # 66 Vacc Suits
+
+# set the Rich world Resale mods
+richWorldResaleDM[1]  <-  3 # 11 Textiles
+richWorldResaleDM[2]  <-  3 # 12 Polymers
+richWorldResaleDM[3]  <-  2 # 13 Liquor
+richWorldResaleDM[4]  <-  2 # 14 Wood
+richWorldResaleDM[5]  <-  3 # 15 Crystals
+richWorldResaleDM[6]  <- -4 # 16 Radioactives
+richWorldResaleDM[7]  <- -1 # 21 Steel
+richWorldResaleDM[8]  <- -1 # 22 Copper
+richWorldResaleDM[9]  <- -1 # 23 Aluminium
+richWorldResaleDM[10] <- -1 # 24 Tin
+richWorldResaleDM[11] <- -1 # 25 Silver
+richWorldResaleDM[12] <- -1 # 26 Special Alloys
+richWorldResaleDM[16] <-  2 # 34 Spices
+richWorldResaleDM[18] <-  4 # 36 Pharmaceuticals
+richWorldResaleDM[19] <-  8 # 41 Gems
+richWorldResaleDM[20] <- -1 # 42 Firearms
+richWorldResaleDM[21] <- -1 # 43 Ammunition
+richWorldResaleDM[22] <- -1 # 44 Blades
+richWorldResaleDM[23] <- -1 # 45 Tools
+richWorldResaleDM[24] <-  1 # 46 Body Armour
+richWorldResaleDM[29] <-  1 # 55 AFV
+
+# set the Poor world Resale mods
+poorWorldResaleDM[7]  <-  3 # 21 Steel
+poorWorldResaleDM[15] <-  1 # 33 Meat
+poorWorldResaleDM[16] <-  3 # 34 Spices
+poorWorldResaleDM[17] <-  2 # 35 Fruit
+poorWorldResaleDM[20] <-  3 # 42 Firearms
+poorWorldResaleDM[21] <-  3 # 43 Ammunition
+poorWorldResaleDM[22] <-  3 # 44 Blades
+poorWorldResaleDM[23] <-  3 # 45 Tools
+poorWorldResaleDM[24] <-  4 # 46 Body Armour
+poorWorldResaleDM[25] <-  1 # 51 Aircraft
+poorWorldResaleDM[26] <-  1 # 52 Air/Raft
+poorWorldResaleDM[27] <-  1 # 53 Computers
+poorWorldResaleDM[28] <-  1 # 54 ATV
+poorWorldResaleDM[30] <-  4 # 56 Farm Machinery
+poorWorldResaleDM[31] <-  4 # 61 Electronic Parts
+poorWorldResaleDM[36] <-  1 # 66 Vacc Suit
+} # End of Resale DMs
+
+testDF <- data.frame(agrWorldPurchDM, 
+                     nonAgrWorldPurchDM, 
+                     indWorldPurchDM, 
+                     nonIndWorldPurchDM, 
+                     richWorldPurchDM, 
+                     poorWorldPurchDM, 
+                     agrWorldResaleDM, 
+                     nonAgrWorldResaleDM, 
+                     indWorldResaleDM, 
+                     nonIndWorldResaleDM, 
+                     richWorldResaleDM, 
+                     poorWorldResaleDM, 
+                     row.names = tradeGood)
